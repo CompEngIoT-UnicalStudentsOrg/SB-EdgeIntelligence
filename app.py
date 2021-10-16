7001534e64f54eac2bfbac53c865a4d8fb8780e4
mar <gim.marcello@gmail.com>
2021-10-16 11:34:59 +0200

    https://github.com/CompEngIoT-UnicalStudentsOrg/SB-EdgeIntelligence/issues/1 - Add an http interface to call the predict method
    - Dockerfile: added more explicit error messages for the API endpoints

--- app.py
+++ app.py
@@ -16,13 +16,15 @@ hum_input_size = 36
 elem_per_row = 7
 
 
-def validate_entry(num_rows: int, rows: list[list[float]]) -> bool:
-    if len(rows) != num_rows:
-        return False
+def validate_entry(num_rows: int, rows: list[list[float]]) -> tuple(bool, str):
+    actual_len = len(rows)
+    if actual_len != num_rows:
+        return False, f"expected #total-rows: {num_rows}, got {actual_len}"
     
-    for row in rows:
-        if len(row) != elem_per_row:
-            return False
+    for idx, row in enumerate(rows):
+        row_len = len(row)
+        if row_len != elem_per_row:
+            return False, f"expected row-#{idx}-length: {elem_per_row}, got {row_len}"
     
     return True
 
@@ -37,8 +39,9 @@ class TemperaturePredicter(Resource):
         "last_data": fields.List(fields.List(fields.Float), required=True)
     })
     def post(self, args):
-        if not validate_entry(temp_input_size, args["last_data"]):
-            return {"error": "input data has invalid dimensions"}, 422
+        valid, msg = validate_entry(temp_input_size, args["last_data"])
+        if not valid:
+            return {"error": "input data has invalid dimensions; " + msg}, 422
         
         data = args["last_data"]
         norm_output = farenheit_to_celsius(predict_temperature(data))
@@ -51,8 +54,9 @@ class HumidityPredicter(Resource):
         "last_data": fields.List(fields.List(fields.Float), required=True)
     })
     def post(self, args):
-        if not validate_entry(hum_input_size, args["last_data"]):
-            return {"error": "input data has invalid dimensions"}, 422
+        valid, msg = validate_entry(hum_input_size, args["last_data"])
+        if not valid:
+            return {"error": "input data has invalid dimensions;" + msg}, 422
         
         data = args["last_data"]
         norm_output = predict_temperature(data)
